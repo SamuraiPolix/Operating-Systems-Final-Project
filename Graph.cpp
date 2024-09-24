e593b23713d85c103d3d05b1da0d820ef9a38616
SamuraiPolix <sam.lazareanu@gmail.com>
2024-09-24 21:35:59 +0300

    Added Kruskal's algo to check for connectivity in directed graph & get functions

--- Graph.cpp
+++ Graph.cpp
@@ -1,8 +1,11 @@
+#include <iostream>
 #include "Graph.hpp"
+#include <stack>
 
 Graph::Graph(int num_vertices) {
     this->num_vertices = num_vertices;
     adj.resize(num_vertices);
+    adjT.resize(num_vertices);
 
     #ifdef DEBUG
         std::cout << "Graph initialized with " << num_vertices << " vertices" << std::endl;
@@ -14,10 +17,117 @@ Graph::~Graph() {
     // adj.clear();
 }
 
-void Graph::addEdge(int u, int v, int weight) {
-    adj[u].push_back(Edge(u, v, weight));
-    adj[v].push_back(Edge(u, v, weight));
+void Graph::resetGraph(int num_vertices) {
+    this->num_vertices = num_vertices;
+    adj.clear();
+    adjT.clear();
+    adj.resize(num_vertices);
+    adjT.resize(num_vertices);
 }
+
+void Graph::addEdge(int u, int v, int weight) {
+    // DIRECTED GRAPH
+    adj[u].push_back(Edge(u, v, weight));
+    adjT[v].push_back(Edge(v, u, weight));
+}
+
+void Graph::removeEdge(int u, int v) {
+    // DIRECTED GRAPH
+    for (auto it = adj[u].begin(); it != adj[u].end(); ++it) {
+        if (it->v == v) {
+            adj[u].erase(it);
+            break;
+        }
+    }
+}
+
+std::vector<Edge> Graph::getEdges() const {
+    std::vector<Edge> edges;
+    for (int i = 0; i < num_vertices; ++i) {
+        for (Edge edge : adj[i]) {
+            edges.push_back(edge);
+        }
+    }
+    return edges;
+}
+
+Edge& Graph::getEdge(int u, int v) {
+    for (Edge& edge : adj[u]) {
+        if (edge.v == v) {
+            return edge;
+        }
+    }
+    throw std::out_of_range("Edge does not exist");
+}
+
+std::vector<Edge> Graph::getNeighbors(int u) const {
+    return adj[u];
+}
+
+bool Graph::isConnected() {
+    return this->Kosaraju();
+}
+
+// DFS function for a graph
+void Graph::DFS(int v, std::vector<bool>& visited, std::stack<int>& stack) {
+    visited[v] = true;
+
+    // Visit all neighbors of v
+    for (const Edge& edge : adj[v]) {
+        if (!visited[edge.v]) {
+            DFS(edge.v, visited, stack);
+        }
+    }
+
+    // Store the vertex after the DFS finishes
+    stack.push(v);
+}
+
+// DFS function for the reversed graph
+void Graph::reverseDFS(int v, std::vector<bool>& visited) {
+    visited[v] = true;
+
+    // Visit all neighbors in the reversed graph
+    
+    for (const Edge& edge : adjT[v]) {
+
+        if (!visited[edge.v]) {
+            reverseDFS(edge.v, visited);
+        }
+    }
+}
+
+bool Graph::Kosaraju() {
+    // Step 1: Perform DFS on the original graph
+    std::stack<int> stack;
+    std::vector<bool> visited(this->getNumVertices(), false);
+
+    // Perform DFS from the first vertex
+    DFS(0, visited, stack);
+
+    // Check if all vertices were visited
+    for (int i = 0; i < this->getNumVertices(); i++) {
+        if (!visited[i]) {
+            return false; // Not strongly connected
+        }
+    }
+
+    // Step 2: Perform DFS on the reversed graph
+    visited.assign(this->getNumVertices(), false); // Reset visited array
+
+    // Perform DFS on the vertex with the highest finishing time in original graph
+    reverseDFS(stack.top(), visited);
+
+    // Check if all vertices were visited in the reversed graph
+    for (int i = 0; i < this->getNumVertices(); i++) {
+        if (!visited[i]) {
+            return false; // Not strongly connected
+        }
+    }
+
+    return true; // Graph is strongly connected
+}
+
 
 void Graph::generateRandomEdges(int num_edges, unsigned int seed) {
     srand(seed);
@@ -38,6 +148,18 @@ void Graph::generateRandomEdges(int num_edges, unsigned int seed) {
     #ifdef DEBUG
         std::cout << "Generated " << num_edges << " random edges" << std::endl;
     #endif
+}
+
+int Graph::getNumVertices() const {
+    return num_vertices;
+}
+
+int Graph::getNumEdges() const {
+    int edge_count = 0;
+    for (int i = 0; i < num_vertices; ++i) {
+        edge_count += adj[i].size();
+    }
+    return edge_count;
 }
 
 void Graph::show(){
--- Graph.hpp
+++ Graph.hpp
@@ -5,33 +5,57 @@
 #include <algorithm>
 #include <iostream>
 #include <fstream>      // for the show() function
+#include <stack>
 
 // Edge struct to simplify working with edges - better in terms of readability
 // Not really necessary, we could use std::tuple<int, int, int> instead for all 3 values (which might have been better in terms of memory)
 struct Edge {
     int u, v, weight;
     Edge(int u, int v, int weight) : u(u), v(v), weight(weight) {}
+
+    // Overload the == operator to compare edges
+    bool operator==(const Edge& other) const {
+        return u == other.u && v == other.v && weight == other.weight;
+    }
+
+    bool operator==(const int& other) const {
+        return u == other || v == other;
+    }
+
+    inline friend std::ostream& operator<<(std::ostream& os, const Edge& edge) {
+        os << edge.u << " -> " << edge.v << " (" << edge.weight << ")";
+        return os;
+    }
 };
-
-std::ostream& operator<<(std::ostream& os, const Edge& edge) {
-    os << edge.u << " -> " << edge.v << " (" << edge.weight << ")";
-    return os;
-}
-
 
 // Weighted, directed graph class
 class Graph {
 private:
     int num_vertices;                     // Number of vertices in the graph
     std::vector<std::vector<Edge>> adj;  // Adjacency list for each vertex
+    std::vector<std::vector<Edge>> adjT; // Transposed adjacency list for Kosaraju's algorithm
     
 public:
     // Constructor to init a graph with the given number of vertices (no edges yet)
     Graph(int num_vertices);
     ~Graph();
 
+    // Reset the graph with the given number of vertices
+    void resetGraph(int num_vertices);
+
     // Add an undirected edge between vertices u and v
     void addEdge(int u, int v, int weight);
+
+    // Remove an edge between vertices u and v
+    void removeEdge(int u, int v);
+
+    // Get edges for algorithms
+    std::vector<Edge> getEdges() const;
+
+    // Get neighbors of a vertex
+    std::vector<Edge> getNeighbors(int u) const;
+
+    Edge& getEdge(int u, int v);
 
     // Generate random edges for the graph based on the given number of edges and seed
     void generateRandomEdges(int num_edges, unsigned int seed);
@@ -39,13 +63,21 @@ public:
     // Check if the graph is connected
     bool isConnected();
 
+    // Get the number of vertices in the graph
+    int getNumVertices() const;
+
+    // Get the number of edges in the graph
+    int getNumEdges() const;
+
     // Show the graph in a GUI using python script
     // TODO DELETE?
     void show();
 
 private:
-    // Helper DFS function to visit all vertices connected to v
-    // void DFS(int v, std::vector<bool>& visited);
+    // Helper DFS functions to visit all vertices in directed graph - using Kosaraju's algorithm
+    void DFS(int v, std::vector<bool>& visited, std::stack<int>& stack);
+    void reverseDFS(int v, std::vector<bool>& visited);
+    bool Kosaraju();
 };
 
 
